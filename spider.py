2695ffd2738484847b770fc5faf0cf6fb681f577
emo <tickbg@gmail.com>
2017-04-05 10:56:50 +0300

    Using regex to extract genres

--- spider.py
+++ spider.py
@@ -11,87 +11,54 @@ from HTMLParser import HTMLParser
 BASE_URL = 'https://yesmovies.to'
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 
-class MyHTMLParser(HTMLParser):
-    def __init__(self, state):
+def get_headers_list():
+    return { 'User-Agent' : USER_AGENT }
+
+
+class HTMLTagParser(HTMLParser):
+    def __init__(self, caller):
         HTMLParser.__init__(self)
-        self.found_genres = False
         self._tag = None
         self._attrs = {}
-        self._state = state
-
-    def check_tag_attr(self, tagname, attrname, attrval):        
-        if self._tag == tagname:
-            if attrname in self._attrs:
-                if self._attrs[attrname] == attrval:
-                    return True
-        return False
-
-    def get_tag_attr(self, tagname, attrname):
-        if self._tag == tagname and attrname in self._attrs:
-            return self._attrs[attrname]
-        return None
+        self._caller = caller
 
     def handle_starttag(self, tag, attrs):
         self._tag = tag
         self._attrs.clear()
         self._attrs = {name: value for name, value in attrs}
-        self._state.on_starttag(tag, self)
-
-    def handle_endtag(self, tag):
-        self._state.on_endtag(tag, self)
-
-    def handle_data(self, data):
-        # print "Encountered some data  :", data
-        pass
+        self._caller(tag, self._attrs)
 
 
-class ParserState(object):
+class SpiderEngine(object):
     def __init__(self):
-        self.found_genres = False
-        self.found_country = False
-        self.sub_menu = False
-        self.
-        self.genres = []
-        self.genres_urls = []
+        self.genres = {}
 
-    def on_starttag(self, tag, parser):
-        if parser.check_tag_attr('a', 'title', 'Genre'):
-            self.found_genres = True
-            return
-        if parser.check_tag_attr('ul', 'class', 'sub-menu') and self.found_genres:
-            self.sub_menu = True
-            return
-        if self.found_genres and self.sub_menu:
-            genre = parser.get_tag_attr('a', 'title')
-            if genre:
-                self.genres.append(genre)
-            link = parser.get_tag_attr('a', 'href')
-            if link:
-                self.genres_urls.append(link)
+    def get_genres(self):
+        if len(self.genres):
+            return self.genres
+        try:
+            req = urllib2.Request(BASE_URL, None, get_headers_list())
+            response = urllib2.urlopen(req)
+            tagparser = HTMLTagParser(self.extract_genre)
+            response_data = response.read()
+            response.close()
+            tagparser.feed(response_data)
+            return self.genres
+        except urllib2.URLError, e:
+            if e.code == 403 or e.code == 401:
+                print('Got error response code')
+        return self.genres
 
-    def on_endtag(self, tag, parser):
-        if self.found_genres and tag == 'ul':
-            self.found_genres = False
+    def extract_genre(self, tag, attrs):
+        if tag == 'a' and 'title' in attrs and 'href' in attrs:
+            mch = re.match(BASE_URL + '/genre\S+' , attrs['href'])
+            if mch:
+                self.genres[attrs['href']] = attrs['title']
 
-
-def get_headers_list():
-    return { 'User-Agent' : USER_AGENT }
 
 def main():
-    try:
-        req = urllib2.Request(BASE_URL, None, get_headers_list())
-        response = urllib2.urlopen(req)
-        state = ParserState()
-        parser = MyHTMLParser(state)
-        response_data = response.read()
-        parser.feed(response_data)
-        print(state.genres)
-        print(state.genres_urls)
-        response.close()
-    except urllib2.URLError, e:
-        if e.code == 403 or e.code == 401:
-            print('Got error response code')
-
+    spider = SpiderEngine()
+    print(spider.get_genres())
 
 if __name__ == '__main__':
     main()
