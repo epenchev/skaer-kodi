cf96e4f7503338b906a71a3b26099010f2d3b495
emo <tickbg@gmail.com>
2017-04-10 23:17:04 +0300

    Executing javascript code from python with nodejs

--- spider.py
+++ spider.py
@@ -5,6 +5,7 @@ import re
 import sys
 import urllib, urllib2
 import cookielib
+import subprocess
 
 BASE_URL = 'https://yesmovies.to'
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
@@ -45,12 +46,33 @@ class SpiderEngine(object):
             if e.code == 403 or e.code == 401:
                 print('Got error response code')
 
+    def get_movie_token(self, url):
+        try:
+            req = urllib2.Request(url, None, get_headers_list())
+            response = urllib2.urlopen(req)
+            jscript = response.read()
+            if response.info().get('Content-Encoding') == 'gzip':
+                print('Content is gziped')
+                buf = StringIO(response.read())
+                f = gzip.GzipFile(fileobj=buf)
+                jscript = f.read()
+            response.close()
+            f = open('movtoken.js', 'w')
+            f.write(jscript + ' console.log(_x); console.log(_y);')
+            f.close()
+            res = subprocess.check_output(['./node', 'movtoken.js'])
+            print(res)
+        except urllib2.URLError, e:
+            if e.code == 403 or e.code == 401:
+                print('Got error response code')
+
 
 def main():
     spider = SpiderEngine()
     genres = spider.get_genres()
     spider.get_movie_list(genres['Kungfu'])
     print(spider.movies)
+    spider.get_movie_token('https://yesmovies.to/ajax/movie_token?eid=613788&mid=20209&_=1491854800884')
 
 if __name__ == '__main__':
     main()
