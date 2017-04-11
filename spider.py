22d47d96e1410da53d1444f685d2e0edf06e9006
emo <tickbg@gmail.com>
2017-04-11 12:26:27 +0300

    Replace nodejs with js2py

--- spider.py
+++ spider.py
@@ -5,13 +5,31 @@ import re
 import sys
 import urllib, urllib2
 import cookielib
-import subprocess
+import js2py
+import sys
 
 BASE_URL = 'https://yesmovies.to'
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 
 def get_headers_list():
     return { 'User-Agent' : USER_AGENT }
+
+def execjs(jsbuf):
+    rcrlm = sys.getrecursionlimit()
+    sys.setrecursionlimit(10000)
+    try:
+        jsfname = 'sample.js'
+        jsfile = open(jsfname, 'w')
+        jsfile.write(jsbuf)
+        jsfile.close()
+        eval_result, resobj = js2py.run_file(jsfname)
+        os.remove(jsfname)
+        sys.setrecursionlimit(rcrlm)
+        return resobj
+    except:
+        print('Error executing javascript')
+    return None
+
 
 class SpiderEngine(object):
     def __init__(self):
@@ -48,20 +66,17 @@ class SpiderEngine(object):
 
     def get_movie_token(self, url):
         try:
-            req = urllib2.Request(url, None, get_headers_list())
-            response = urllib2.urlopen(req)
-            jscript = response.read()
-            if response.info().get('Content-Encoding') == 'gzip':
-                print('Content is gziped')
-                buf = StringIO(response.read())
-                f = gzip.GzipFile(fileobj=buf)
-                jscript = f.read()
-            response.close()
-            f = open('movtoken.js', 'w')
-            f.write(jscript + ' console.log(_x); console.log(_y);')
-            f.close()
-            res = subprocess.check_output(['./node', 'movtoken.js'])
-            print(res)
+            exec_retry = 5
+            while exec_retry:
+                req = urllib2.Request(url, None, get_headers_list())
+                response = urllib2.urlopen(req)
+                res = execjs(response.read())
+                response.close()
+                exec_retry -= 1
+                if res:
+                    print(res._x)
+                    print(res._y)
+                    break
         except urllib2.URLError, e:
             if e.code == 403 or e.code == 401:
                 print('Got error response code')
@@ -72,7 +87,7 @@ def main():
     genres = spider.get_genres()
     spider.get_movie_list(genres['Kungfu'])
     print(spider.movies)
-    spider.get_movie_token('https://yesmovies.to/ajax/movie_token?eid=613788&mid=20209&_=1491854800884')
+    spider.get_movie_token('https://yesmovies.to/ajax/movie_token?eid=613788&mid=20209')
 
 if __name__ == '__main__':
     main()
