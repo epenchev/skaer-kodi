2a96a1cfabcdfd722df3879aadfd404af8f4809c
emo <tickbg@gmail.com>
2017-08-02 17:51:59 +0300

    Add Kodi addon files

--- skaer.py
+++ skaer.py
@@ -4,12 +4,12 @@ from lib import scraper
 
 def main():
     s = scraper.MediaScraper()
-    # genres = s.avail_genres()
-    # movies = s.movies_info(genres['Action'])
-    # plist = s.media_url(movies.iteritems().next())
-    # print(plist)
-    res = s.search("Rambo")
-    print(res)
+    genres = s.avail_genres()
+    movies = s.movies_info(genres['Action'])
+    plist = s.media_url(movies.iteritems().next())
+    print(plist)
+    # res = s.search("Rambo")
+    # print(res)
 
 if __name__ == '__main__':
     main()
