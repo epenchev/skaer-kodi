e7b4f623c3b41c4478993f388c097d7ee7fee3fe
emo <tickbg@gmail.com>
2017-08-04 11:39:14 +0300

    Created example gui for the kodi plugin

--- addon.py
+++ addon.py
@@ -1,1 +1,25 @@
 __author__ = 'tickbg'
+
+import xbmc, xbmcgui, xbmcplugin, xbmcaddon, xbmcvfs
+import sys
+
+addon = xbmcaddon.Addon(id='plugin.video.skaer')
+plugin_handle = int(sys.argv[1]) if len(sys.argv) > 1 else None
+
+def create_items():
+    listing = []
+    listing.append( 'The first item' )
+    listing.append( 'The second item' )
+    listing.append( 'The third item' )
+    listing.append( 'The fourth item' )
+    return listing
+
+def kodigui_run(gui_items):
+    global plugin_handle
+    for item in gui_items:
+        list_item = xbmcgui.ListItem(item)
+        xbmcplugin.addDirectoryItem(plugin_handle, '', list_item)
+        # tell kodi we have finished creating the directory listing
+        xbmcplugin.endOfDirectory(plugin_handle)
+
+kodigui_run(create_items())
